b4e9cf0155097c072adcce27b93e687c9788e7f9
nico8034 <nicolaika8034@gmail.com>
2022-11-02 00:27:02 +0100

    working on frontend

--- client/app.js
+++ client/app.js
@@ -1,126 +1,94 @@
-const Web3 = require('web3');
-const TruffleContract = require('truffle-contract');
-const request = require('request');
-App = {
-  /*==================================================================
-      DEFINE ESSENTIALS
-  ==================================================================*/
-  web3Provider: null,
-  contracts: {},
-  currentAccount: {},
 
-  // Integrates web3 with our web app
-  initWeb3: async function () {
-    if (process.env.MODE == 'development' || typeof window.web3 === 'undefined') {
-      App.web3Provider = new Web3.providers.HttpProvider(process.env.LOCAL_NODE);
-    }
-    else {
-      App.web3Provider = web3.currentProvider;
-    }
-    web3 = new Web3(App.web3Provider);
-    return await App.initContract();
+const web3 = new Web3("http://127.0.0.1:8543")
+const Bank = [
+  {
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
   },
-
-  // Initialises a variable with IDManagement contract JSON
-  initContract: async function () {
-    await $.getJSON('HelloWorld.json', function (data) {
-      var hwArtifact = data;
-      App.contracts.helloworld = TruffleContract(hwArtifact);
-      App.contracts.helloworld.setProvider(App.web3Provider);
-    })
-    return App.bindEvents();
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
   },
-
-  // Binds button clicks to the repective functions
-  bindEvents: function () {
-    $('#setName').click(App.ExeInputUser);
-    $('#getName').click(App.CallDispUser);
-  },
-  // Defines functionality for OUTPUT label
-  showMessage: function (msg) {
-    $('#output').html(msg.toString());
-    $('#output').show();
-    $('#errorHolder').hide();
-  },
-
-  // Defines functionality for ERROR label
-  showError: function (err) {
-    $('#errorHolder').html(err.toString());
-    $('#errorHolder').show();
-    $('#output').hide();
-  },
-  /*==================================================================
-      USERNAME SUBMISSION
-  ==================================================================*/
-  ExeInputUser: function () {
-    var NAME = $('#username').val();
-    console.log("In app.js ExeInputUser", NAME);
-
-    if (NAME) {
-      // Retrieves user account to perform operations
-      web3.eth.getAccounts(function (error, accounts) {
-        if (error) {
-          console.log("ERROR getAccounts ExeInputUser");
-          App.showError(error);
-        }
-        App.currentAccount = accounts[0];
-
-        // Submits name to the blockchain network
-        App.contracts.helloworld.deployed().then(function (obj) {
-          return obj.inputUser.sendTransaction(NAME, { from: App.currentAccount });
-        }).then(function (result) {
-          console.log(result);
-          App.showMessage("Name submitted as a txn");
-        }).catch(function (error) {
-          console.log("ERROR ExeInputUser");
-          App.showError(error);
-        });
-      });
-    }
-    else {
-      App.showError("Valid name is required !");
-    }
-  },
-  /*==================================================================
-   USERNAME RETRIEVAL
-  ==================================================================*/
-  CallDispUser: function () {
-    var NAME = $('#username').val();
-    if (NAME) {
-      console.log("In app.js CallDispUser");
-      web3.eth.getAccounts(function (error, accounts) {
-        if (error) {
-          console.log("ERROR getAccounts CallDispUser");
-          App.showError(error);
-        }
-        App.currentAccount = accounts[0];
-
-        App.contracts.helloworld.deployed().then(function (instance) {
-          return instance.dispUser.call({ from: App.currentAccount });
-        }).then(function (result) {
-          console.log("CallDispUser returns : ", result);
-          App.showMessage(result);
-        }).catch(function (error) {
-          console.log("ERROR CallRegStatus");
-          App.showError(error);
-        })
-      })
-    }
-    else {
-      App.showError("No output as no name is entered");
-    }
-  },
-  /*==================================================================
-   INITIALISATION : Intialises web3 when webpage is loaded onto browser
-  ==================================================================*/
-  init: async function () {
-    await App.initWeb3();
-    console.log("In app.js init, initiated App");
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   }
+];
+
+let accounts;
+
+async function getBlockNumber() {
+  const latestBlockNumber = await web3.eth.getBlockNumber()
+  console.log(latestBlockNumber)
+  return latestBlockNumber
 }
 
-$(function () {
-  $(window).load(function () {
-    App.init();
-  });
-});
+//getBlockNumber()
+
+const createAccount = async () => {
+  let accountNameElement = document.getElementById("accountName");
+  let accountBalanceElement = document.getElementById("accountBalance");
+  let account = await web3.eth.accounts.create("seed2");
+  let balance = await web3.eth.getBalance(account.address)
+  accountNameElement.innerHTML = account.address;
+  accountBalanceElement.innerHTML = web3.utils.fromWei(balance, 'ether');
+}
+
+const helloWorld = () => {
+  let myContract = web3.eth
+  console.log(web3.eth.Contract)
+}
+
+const getAccounts = () => {
+  accounts = web3.eth.getAccounts().then(console.log)
+}
+
+const getBalanceOfAccount = async () => {
+  let addressInput = document.getElementById("addressInput").value;
+  let balance = await web3.eth.getBalance(addressInput)
+  document.getElementById("balanceForAddress").innerHTML = web3.utils.fromWei(balance, 'ether');
+}
+
+const deployedContract = async () => {
+  let deployer = "0x1d43faC70f3471bC2f8aF3784F251bF738321616";
+  let contractAddress = "0xB4e5953FF3BB7780d528B9CFE8C4D77691CF8f28";
+  let contract = new web3.eth.Contract(Bank, contractAddress);
+  let amount = web3.utils.toWei("1", 'ether');
+
+  console.log(contract.methods);
+  contract.methods.deposit({
+    from: deployer,
+    value: amount
+  })
+  contract.methods.balanceOf(deployer).call().then(console.log)
+
+}
+
+
